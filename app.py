0aa2031fa9b01dbc30dc1c70f7507277f83bcf48
GarciaDos <ninogarcia3209@gmail.com>
2024-03-24 20:40:48 +0800

    Pushing app.py to be deployed
    - Testing phase on how streamlit works

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def load_data():
+    data = pd.read_csv(
+        "./data/micro_world.csv",
+        encoding='latin-1'
+    )
+    return data
+
+df = pd.read_csv("./data/micro_world.csv",encoding="latin1", engine="python")
+
+st.title("Hello world!") 
+
+fig = sns.histplot(data=df, x='age', bins=30, kde=True)
+plt.title('Distribution of Respondent Ages')
+plt.xlabel('Age')
+plt.ylabel('Frequency')
+st.pyplot(fig.figure)
